0ae7aee9e3cc13d9477aef0b4d2a4dbd3592be60
влад мазяков <mazyakov02@mail.ru>
2021-05-02 00:18:52 +0400

    ready

--- routes/board.js
+++ routes/board.js
@@ -4,7 +4,7 @@ const boardController = require('../controller/board')
 const passport = require('passport')
 
 router.post('/board',passport.authenticate('jwt', { session: false }), boardController.createBoard)
-router.get('/board', passport.authenticate('jwt', { session: false }), boardController.getBoards)
+router.get('/board', boardController.getBoards)
 router.put('/board', passport.authenticate('jwt', { session: false }), boardController.updateBoard)
 router.delete('/board', passport.authenticate('jwt', { session: false }), boardController.deleteBoard)
 
